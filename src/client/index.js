e5f291c9377ef567b648af4dd3f985db79bac1fe
He-zhiyong <15988134176@163.com>
2017-09-02 09:05:56 +0800

    update

--- src/client/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from '../server/registerServiceWorker';
-//import App from './components/App';
-import TodoListApp from '../common/components/TodoListApp';
-
-ReactDOM.render(<TodoListApp/>, document.getElementById('root'));
-registerServiceWorker();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './server/registerServiceWorker';
-import Login from './common/pages/Login';
+import registerServiceWorker from './registerServiceWorker';
+import Login from './pages/Login';
 
 ReactDOM.render(<Login/>, document.getElementById('root'));
 registerServiceWorker();
